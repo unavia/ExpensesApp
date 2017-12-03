fe5f8e240f74d663ec541b2e288e686d8b51fccb
Kendall Roth <kroth@unavia.ca>
2017-12-02 22:12:49 -0500

    #4 Add Toolbar to Settings page

--- app/src/main/java/ca/kendallroth/expensesapp/activities/SettingsActivity.java
+++ app/src/main/java/ca/kendallroth/expensesapp/activities/SettingsActivity.java
@@ -2,6 +2,7 @@ package ca.kendallroth.expensesapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import ca.kendallroth.expensesapp.R;
 import ca.kendallroth.expensesapp.fragments.SettingsFragment;
@@ -16,9 +17,16 @@ public class SettingsActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_settings);
 
+    // Toolbar
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    toolbar.setTitle("Settings");
+
+    setSupportActionBar(toolbar);
+
     // Display the Preferences fragment as the main content.
     getFragmentManager().beginTransaction()
-        .replace(android.R.id.content, new SettingsFragment())
+        // .replace(android.R.id.content, new SettingsFragment())
+        .replace(R.id.settings_content, new SettingsFragment())
         .commit();
   }
 }
